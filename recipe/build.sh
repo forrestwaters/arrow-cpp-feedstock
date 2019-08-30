3ce7496deb7ac4a037df6cd00d7695fbf8166527
Forrest Waters <fwaters@0609.local>
2019-08-30 13:29:41 -0500

    arrow 0.13.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,69 +2,48 @@
 
 set -e
 set -x
-
-# Build dependencies
-export ARROW_BUILD_TOOLCHAIN=$PREFIX
-
-cd cpp
-mkdir build-dir
-cd build-dir
-
-declare -a _CMAKE_EXTRA_CONFIG
+# pulled from https://github.com/AnacondaRecipes/libnetcdf-feedstock/blob/master/recipe/build.sh
+declare -a CMAKE_PLATFORM_FLAGS
 if [[ ${HOST} =~ .*darwin.* ]]; then
-    if [[ ${_XCODE_BUILD} == yes ]]; then
-        _CMAKE_EXTRA_CONFIG+=(-G'Xcode')
-        _CMAKE_EXTRA_CONFIG+=(-DCMAKE_OSX_ARCHITECTURES=x86_64)
-        _CMAKE_EXTRA_CONFIG+=(-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT})
-        _VERBOSE=""
-    fi
-    unset MACOSX_DEPLOYMENT_TARGET
-    export MACOSX_DEPLOYMENT_TARGET
-    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_AR=${AR})
-    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_RANLIB=${RANLIB})
-    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_LINKER=${LD})
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+  export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,-dead_strip_dylibs//g")
+else
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
 
-# CMake has a hard time coping with our hard-coding of c++ standard.  Arrow sets it in cmake (CMAKE_CXX_STANDARD)
-if [[ ${target_platform} =~ .*linux.* ]]; then
-    CXXFLAGS="${CXXFLAGS//-std=c++17/}"
-    # I hate you so much CMake.
-    LIBPTHREAD=$(find ${BUILD_PREFIX} -name "libpthread.so")
-    _CMAKE_EXTRA_CONFIG+=("-DPTHREAD_LIBRARY=${LIBPTHREAD}")
-else
-    CXXFLAGS="${CXXFLAGS//-std=c++14/}"
+if [ "$(uname)" = "Linux" ] ; then
+  export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
 fi
+
+mkdir cpp/build
+pushd cpp/build
 
 cmake \
     -DCMAKE_BUILD_TYPE=release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
+    -DARROW_DEPENDENCY_SOURCE=SYSTEM \
+    -DARROW_PACKAGE_PREFIX=$PREFIX \
     -DARROW_BOOST_USE_SHARED=ON \
+    -DARROW_BUILD_STATIC=OFF \
     -DARROW_BUILD_BENCHMARKS=OFF \
-    -DARROW_BUILD_UTILITIES=ON \
+    -DARROW_BUILD_UTILITIES=OFF \
     -DARROW_BUILD_TESTS=OFF \
     -DARROW_JEMALLOC=ON \
     -DARROW_PLASMA=ON \
     -DARROW_PYTHON=ON \
-    -DARROW_ORC=ON \
     -DARROW_PARQUET=ON \
-    -DBROTLI_HOME=$PREFIX \
-    -DFLATBUFFERS_HOME=$PREFIX \
-    -DLZ4_HOME=$PREFIX \
-    -DRAPIDJSON_HOME=$PREFIX \
-    -DSNAPPY_HOME=$PREFIX \
-    -DZLIB_HOME=$PREFIX \
-    -DZSTD_HOME=$PREFIX \
-    -DTHRIFT_HOME=$PREFIX \
-    -DPROTOBUF_HOME=$PREFIX \
-    -DCMAKE_C_COMPILER=$(type -p ${CC})     \
-    -DCMAKE_CXX_COMPILER=$(type -p ${CXX})  \
-    -DCMAKE_C_FLAGS="$CFLAGS" \
-    -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
-    -DARROW_CXXFLAGS="$CXXFLAGS" \
-    "${_CMAKE_EXTRA_CONFIG[@]}" \
+    -DARROW_GANDIVA=OFF \
+    -DARROW_ORC=ON \
+    -DORC_HOME=$PREFIX \
+    -DCMAKE_AR=${AR} \
+    -DCMAKE_RANLIB=${RANLIB} \
+    -DPYTHON_EXECUTABLE="${PYTHON}" \
+    -DBoost_NO_BOOST_CMAKE=ON \
+    -GNinja \
+    ${CMAKE_PLATFORM_FLAGS[@]} \
     ..
 
-# make VERBOSE=1
-make -j${CPU_COUNT} VERBOSE=1
-make install
+ninja install
+
+popd
